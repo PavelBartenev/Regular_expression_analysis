4b90e67655249e7290e17c8af78ba464f7198ae2
PavelBartenev <56157750+PavelBartenev@users.noreply.github.com>
2020-11-11 19:21:04 +0300

    Update Regexpr.cpp

--- Regexpr.cpp
+++ Regexpr.cpp
@@ -1,64 +1,58 @@
-#include "Word.cpp"
-#include <stack>
+#include "Regexpr.h"
+#include <algorithm>
 #include <string>
+#include "Word.h"
 
-class Regexpr {
-	std::string alphabet;
-	std::string regexpr;
-	std::stack<Word> parsing_stack;
+Regexpr::Regexpr(const std::string& expr, const std::string& alphabet)
+	: regexpr{ expr }, alphabet{ alphabet } {}
 
-public:
-	Regexpr(const std::string& expr, const std::string& alphabet)
-		: regexpr{ expr }, alphabet{ alphabet } {}
+int Regexpr::get_words(Word& left, Word& right) {
+	if (parsing_stack.size() < 2)
+		return -1;
+	right = parsing_stack.top();
+	parsing_stack.pop();
+	left = parsing_stack.top();
+	parsing_stack.pop();
 
-	int get_words(Word& left, Word& right) {
-		if (parsing_stack.size() < 2)
+	return 0;
+}
+
+int Regexpr::longest_Xprefix_len(char x) {
+	for (const auto& symbol : regexpr) {
+		if (alphabet.find(symbol) == std::string::npos)
 			return -1;
-		right = parsing_stack.top();
-		parsing_stack.pop();
-		left = parsing_stack.top();
-		parsing_stack.pop();
 
-		return 0;
-	}
-
-	int longest_Xprefix_len(char x) {
-		for (const auto& symbol : regexpr) {
-			if (alphabet.find(symbol) == std::string::npos)
-				return -1;
-
-			if (symbol == '+') {
-				Word left_word, right_word;
-				if (get_words(left_word, right_word) != -1)
-					parsing_stack.push(left_word + right_word);
-				else
-					return -1;
-			}
-			else if (symbol == '.') {
-				Word left_word, right_word;
-				if (get_words(left_word, right_word) != -1)
-					parsing_stack.push(left_word & right_word);
-				else
-					return -1;
-			}
-			else if (symbol == '*') {
-				if (parsing_stack.size() < 1)
-					return -1;
-				Word word = parsing_stack.top();
-				parsing_stack.pop();
-				parsing_stack.push(*word);
-			}
-			else if (symbol == '1')
-				parsing_stack.push(Word{ 0, 0, true });
-			else if (symbol == x)
-				parsing_stack.push(Word{ 1, 1, false });
+		if (symbol == '+') {
+			Word left_word, right_word;
+			if (get_words(left_word, right_word) != -1)
+				parsing_stack.push(left_word + right_word);
 			else
-				parsing_stack.push(Word{ 0, 0, false });
+				return -1;
 		}
-
-		if (parsing_stack.size() != 1)
-			return -1;
-
-		return std::max(parsing_stack.top().Xprefix_len, parsing_stack.top().Xword_len);
+		else if (symbol == '.') {
+			Word left_word, right_word;
+			if (get_words(left_word, right_word) != -1)
+				parsing_stack.push(left_word & right_word);
+			else
+				return -1;
+		}
+		else if (symbol == '*') {
+			if (parsing_stack.size() < 1)
+				return -1;
+			Word word = parsing_stack.top();
+			parsing_stack.pop();
+			parsing_stack.push(*word);
+		}
+		else if (symbol == '1')
+			parsing_stack.push(Word{ 0, 0, true });
+		else if (symbol == x)
+			parsing_stack.push(Word{ 1, 1, false });
+		else
+			parsing_stack.push(Word{ 0, 0, false });
 	}
-};
+
+	if (parsing_stack.size() != 1)
+		return -1;
+
+	return std::max(parsing_stack.top().Xprefix_len, parsing_stack.top().Xword_len);
+}
