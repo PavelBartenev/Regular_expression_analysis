fec2a02f0e9f15a1563b11e9ec4cbda023b3b926
PavelBartenev <pavelbartenev@mail.ru>
2021-01-04 23:53:42 +0300

    New CMake added

---
+++ Regexpr_analysis/Regular_expression_analysis.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include "Regexpr.h"
+
+int main() {
+	std::string str;
+	std::cin >> str;
+	char symbol;
+	std::cin >> symbol;
+
+	std::string alphabet = { 'a', 'b', 'c', '.', '+', '*', '1' };
+
+	Regexpr expr{ str, alphabet };
+
+	int Xprefix_len = expr.longest_Xprefix_len(symbol);
+
+	if (Xprefix_len == -1)
+		std::cout << "ERROR";
+	else if (Xprefix_len == std::numeric_limits<int>::max())
+		std::cout << "INF";
+	else
+		std::cout << Xprefix_len;
+
+	return 0;
+}
---
+++ Regexpr_analysis_test/Word.cpp
@@ -0,0 +1,42 @@
+#include "Word.h"
+#include <algorithm>
+
+Word::Word() : Xprefix_len{ 0 }, Xword_len{ 0 }, empty{ false } {};
+
+Word::Word(int Xprefix_len, int Xword_len, bool empty) : Xprefix_len{ Xprefix_len }, Xword_len{ Xword_len }, empty{ empty } {};
+
+Word Word::operator +(const Word& right_word) {
+	Word new_word;
+	new_word.Xprefix_len = std::max(Xprefix_len, right_word.Xprefix_len);
+	new_word.Xword_len = std::max(Xword_len, right_word.Xword_len);
+	new_word.empty = empty || right_word.empty;
+
+	return new_word;
+}
+
+Word Word::operator &(const Word& right_word) {
+	Word new_word;
+
+	if ((Xword_len > 0 || empty) && (right_word.Xword_len > 0 || right_word.empty))
+		new_word.Xword_len = Xword_len + right_word.Xword_len;
+	else
+		new_word.Xword_len = 0;
+
+	if (Xword_len > 0 || empty)
+		new_word.Xprefix_len = std::max(Xword_len + right_word.Xprefix_len, Xprefix_len);
+	else
+		new_word.Xprefix_len = 0;
+
+	new_word.empty = empty && right_word.empty;
+
+	return new_word;
+}
+
+Word Word::operator *() {
+	Word new_word;
+	new_word.Xword_len = (Xword_len == 0 ? 0 : std::numeric_limits<int>::max());
+	new_word.Xprefix_len = std::max(new_word.Xword_len, Xprefix_len);
+	new_word.empty = true;
+
+	return new_word;
+}
